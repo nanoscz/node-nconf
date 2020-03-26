00791bc6c71f1d6711e412f0dec55c4939334bbc
fernando <fernandocto.scz@gmail.com>
2020-03-25 23:50:53 -0400

    [ADD] first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const express =  require('express')
+const nconf = require('nconf')
+const proxyFile = JSON.stringify(require('./proxy.conf'))
+const httpProxyMiddleware = require('http-proxy-middleware')
+
+const app = express()
+const settings = nconf.argv()
+     .env()
+     .defaults({ proxies: proxyFile, PORT: 5000 })
+     .get()
+
+const proxies = JSON.parse(settings.proxies)
+Object.keys(proxies).forEach(key => {
+  app.use(key, httpProxyMiddleware(key, proxies[key]))
+})
+
+app.listen(3005, () => {
+  console.log(`server listening on port http://localhost:${settings.PORT}`)
+})
